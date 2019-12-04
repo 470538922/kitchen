eeba1444f251159817b52839b52019701d5f1716
470538922 <470538922@qq.com>
2019-12-04 16:09:07 +0800

    新增二维码

--- src/components/cookbook/index.vue
+++ src/components/cookbook/index.vue
@@ -1,245 +1,301 @@
 <template>
-	<div class="cookbook_list">
-		<div :class="[{hide:isHideList}]">
-			<div class="top_list">
-				<el-button
-					permCode="manu_list_lookup.menu_list_add"
-					banType="disable"
-					size="small"
-					type="primary"
-					icon="el-icon-circle-plus-outline"
-					@click="$router.push({path:'/Cookbook/AddCookbook'})"
-				>添加菜谱</el-button>
-				<el-button
-					icon="el-icon-folder-checked"
-					size="small"
-					type="primary"
-					@click="$router.push({path:'/Cookbook/PrefabricatedRecipes'})"
-				>出厂预置菜谱</el-button>
-			</div>
-			<div class="bottom_list">
-				<div class="top_title">
-					<h4>菜谱列表</h4>
-					<div class="top_search">
-						<el-col :span="20" style="padding:0 5px;">
-							<el-input size="small" clearable placeholder="菜谱名称" v-model="keyword"></el-input>
-						</el-col>
-						<el-col :span="4" style="padding:0 5px;">
-							<el-button size="small" plain @click="getCookbookList">搜索</el-button>
-						</el-col>
-					</div>
-				</div>
-				<div class="table_list">
-					<el-table
-						:data="tableData"
-						style="width: 100%"
-						size="mini"
-						tooltip-effect="light"
-						:header-cell-style="{'background-color':'#eee','color':'#333333', 'font-weight': 'normal'}"
-					>
-						<el-table-column label="菜谱名称" min-width="100" show-overflow-tooltip>
-							<template slot-scope="scope">
-								<span>{{ scope.row.recipeName }}</span>
-							</template>
-						</el-table-column>
-						<el-table-column label="分类" min-width="80" show-overflow-tooltip>
-							<template slot-scope="scope">
-								<span>{{ scope.row.cateName }}</span>
-							</template>
-						</el-table-column>
-						<el-table-column label="售价（元）" min-width="80" show-overflow-tooltip>
-							<template slot-scope="scope">
-								<span>￥{{scope.row.recipePrice/100}}</span>
-							</template>
-						</el-table-column>
-						<el-table-column label="*上/下架" min-width="80">
-							<template slot-scope="scope">
-								<span>
-									<el-button
-										permCode="manu_list_lookup.menu_list_add"
-										banType="disable"
-										type="text"
-										v-if="scope.row.state=='0'"
-										@click.stop.prevent="changeUp(scope.$index, scope.row)"
-									>
-										<i class="iconfont" style="color:green;">&#xe659;</i>
-									</el-button>
-									<el-button
-										permCode="manu_list_lookup.menu_list_add"
-										banType="disable"
-										type="text"
-										v-if="scope.row.state=='1'"
-										@click.stop.prevent="changeUp(scope.$index, scope.row)"
-									>
-										<i class="iconfont" style="color:red;">&#xe658;</i>
-									</el-button>
-								</span>
-							</template>
-						</el-table-column>
-						<el-table-column label="操作" width="100">
-							<template slot-scope="scope">
-								<el-button
-									permCode="manu_list_lookup.menu_list_update"
-									banType="disable"
-									type="text"
-									size="mini"
-									@click.stop.prevent="handleEdit(scope.$index, scope.row)"
-								>修改</el-button>
-							</template>
-						</el-table-column>
-					</el-table>
-				</div>
-				<div class="block" style="margin-top:10px;float:right">
-					<el-pagination
-						background
-						@size-change="handleSizeChange"
-						@current-change="handleCurrentChange"
-						:current-page.sync="pageIndex"
-						:page-sizes="[10, 20,40, 100]"
-						:page-size="pageSize"
-						layout="total, sizes, prev, pager, next, jumper"
-						:total="total"
-					></el-pagination>
-				</div>
-			</div>
-		</div>
-		<router-view></router-view>
-	</div>
+  <div class="cookbook_list">
+    <div :class="[{hide:isHideList}]">
+      <div class="top_list">
+        <el-button
+          permCode="manu_list_lookup.menu_list_add"
+          banType="disable"
+          size="small"
+          type="primary"
+          icon="el-icon-circle-plus-outline"
+          @click="$router.push({path:'/Cookbook/AddCookbook'})"
+        >添加菜谱</el-button>
+        <el-button
+          icon="el-icon-folder-checked"
+          size="small"
+          type="primary"
+          @click="$router.push({path:'/Cookbook/PrefabricatedRecipes'})"
+        >出厂预置菜谱</el-button>
+      </div>
+      <div class="bottom_list">
+        <div class="top_title">
+          <h4>菜谱列表</h4>
+          <div class="top_search">
+            <el-col :span="20" style="padding:0 5px;">
+              <el-input size="small" clearable placeholder="菜谱名称" v-model="keyword"></el-input>
+            </el-col>
+            <el-col :span="4" style="padding:0 5px;">
+              <el-button size="small" plain @click="getCookbookList">搜索</el-button>
+            </el-col>
+          </div>
+        </div>
+        <div class="table_list">
+          <el-table
+            :data="tableData"
+            style="width: 100%"
+            size="mini"
+            tooltip-effect="light"
+            :header-cell-style="{'background-color':'#eee','color':'#333333', 'font-weight': 'normal'}"
+          >
+            <el-table-column label="菜谱名称" min-width="100" show-overflow-tooltip>
+              <template slot-scope="scope">
+                <span>{{ scope.row.recipeName }}</span>
+              </template>
+            </el-table-column>
+            <el-table-column label="分类" min-width="80" show-overflow-tooltip>
+              <template slot-scope="scope">
+                <span>{{ scope.row.cateName }}</span>
+              </template>
+            </el-table-column>
+            <el-table-column label="售价（元）" min-width="80" show-overflow-tooltip>
+              <template slot-scope="scope">
+                <span>￥{{scope.row.recipePrice/100}}</span>
+              </template>
+            </el-table-column>
+            <el-table-column label="*上/下架" min-width="80">
+              <template slot-scope="scope">
+                <span>
+                  <el-button
+                    permCode="manu_list_lookup.menu_list_add"
+                    banType="disable"
+                    type="text"
+                    v-if="scope.row.state=='0'"
+                    @click.stop.prevent="changeUp(scope.$index, scope.row)"
+                  >
+                    <i class="iconfont" style="color:green;">&#xe659;</i>
+                  </el-button>
+                  <el-button
+                    permCode="manu_list_lookup.menu_list_add"
+                    banType="disable"
+                    type="text"
+                    v-if="scope.row.state=='1'"
+                    @click.stop.prevent="changeUp(scope.$index, scope.row)"
+                  >
+                    <i class="iconfont" style="color:red;">&#xe658;</i>
+                  </el-button>
+                </span>
+              </template>
+            </el-table-column>
+            <el-table-column label="操作" width="140">
+              <template slot-scope="scope">
+                <el-button
+                  permCode="manu_list_lookup.menu_list_update"
+                  banType="disable"
+                  type="text"
+                  size="mini"
+                  @click.stop.prevent="handleEdit(scope.$index, scope.row)"
+                >修改</el-button>
+                <el-button
+                  permCode="manu_list_lookup.menu_list_update"
+                  banType="disable"
+                  type="text"
+                  size="mini"
+                  @click.stop.prevent="shoWerweima(scope.$index, scope.row)"
+                >二维码</el-button>
+              </template>
+            </el-table-column>
+          </el-table>
+        </div>
+        <div class="block" style="margin-top:10px;float:right">
+          <el-pagination
+            background
+            @size-change="handleSizeChange"
+            @current-change="handleCurrentChange"
+            :current-page.sync="pageIndex"
+            :page-sizes="[10, 20,40, 100]"
+            :page-size="pageSize"
+            layout="total, sizes, prev, pager, next, jumper"
+            :total="total"
+          ></el-pagination>
+        </div>
+      </div>
+      <el-dialog :show-close="false" :visible.sync="erweimaVisible" class="showPic" width="400px">
+        <img width="100%" :src="erweima" alt />
+        <div style="padding:8px;text-align:center">
+          <el-button size="mini" @click="download">下载</el-button>
+        </div>
+      </el-dialog>
+    </div>
+    <router-view></router-view>
+  </div>
 </template>
 <script>
 import tableList from "../public/table";
 export default {
-	inject: ["reload"],
-	data() {
-		return {
-			currentPage: 1,
-			items: [],
-			tableData: [],
-			pageIndex: 1,
-			pageSize: 10,
-			total: 10,
-			keyword: "",
-			isHideList: this.$route.params.id !== undefined ? true : false
-		};
-	},
-	methods: {
-		changeState(val) {
-			let qs = require("qs");
-			let data = qs.stringify({
-				id: val.id
-			});
-			this.Axios(
-				{
-					params: data,
-					option: {
-						successMsg: "修改成功"
-					},
-					type: "post",
-					url: "/api-recipe/recipe/updatestate",
-					loadingConfig: {
-						target: document.querySelector(".cookbook_list")
-					}
-				},
-				this
-			).then(
-				result => {
-					if (result.data.code === 200) {
-						this.getCookbookList();
-					}
-				},
-				({ type, info }) => {}
-			);
-		},
-		changeUp(index, val) {
-			if (val.state == 1) {
-				this.changeState(val);
-			} else {
-				this.$confirm("菜谱下架，绑定的商品也同样会下架, 是否继续?", "提示", {
-					confirmButtonText: "确定",
-					cancelButtonText: "取消",
-					type: "warning",
-					cancelButtonClass: "is-plain"
-				})
-					.then(() => {
-						this.changeState(val);
-					})
-					.catch(() => {});
-			}
-		},
-		handleSizeChange(val) {
-			this.pageIndex = 1;
-			this.pageSize = val;
-			this.getCookbookList();
-		},
-		handleCurrentChange(val) {
-			this.pageIndex = val;
-			this.getCookbookList();
-		},
-		handleEdit(index, row) {
-			this.$router.push({
-				path: "/Cookbook/EditCookbook/" + row.id
-			});
-		},
-		handleSelectionChange(selection) {},
-		getRow(row, event) {},
-		getCookbookList() {
-			let data = {
-				page: this.pageIndex,
-				size: this.pageSize,
-				keyword: this.keyword
-			};
-			this.Axios(
-				{
-					params: data,
-					option: {
-						enableMsg: false
-					},
-					type: "get",
-					url: "/api-recipe/recipe/list",
-					loadingConfig: {
-						target: document.querySelector(".cookbook_list")
-					}
-				},
-				this
-			).then(
-				result => {
-					if (result.data.code === 200) {
-						this.total = result.data.data.totalElement;
-						this.tableData = result.data.data.content;
-						this.total = result.data.data.totalElement;
-					}
-				},
-				({ type, info }) => {}
-			);
-		}
-	},
-	created() {
-		this.getCookbookList();
-		let a = this.$route.matched.find(
-			item =>
-				item.name === "AddCookbook" || item.name === "PrefabricatedRecipes"
-		)
-			? true
-			: false;
-		let b = this.$route.params.id !== undefined ? true : false;
-		this.isHideList = a || b ? true : false;
-	},
-	components: {
-		tableList
-	},
-	watch: {
-		$route() {
-			this.reload();
-			let a = this.$route.matched.find(
-				item =>
-					item.name === "AddCookbook" || item.name === "PrefabricatedRecipes"
-			)
-				? true
-				: false;
-			let b = this.$route.params.id !== undefined ? true : false;
-			this.isHideList = a || b ? true : false;
-		}
-	}
+  inject: ["reload"],
+  data() {
+    return {
+      fileName: "",
+      erweimaVisible: false,
+      erweima: "",
+      currentPage: 1,
+      items: [],
+      tableData: [],
+      pageIndex: 1,
+      pageSize: 10,
+      total: 10,
+      keyword: "",
+      isHideList: this.$route.params.id !== undefined ? true : false
+    };
+  },
+  methods: {
+    download() {
+      let qs = require("qs");
+      let value = qs.stringify({
+        baseimg: this.erweima
+      });
+      this.axios
+        .post("http://192.168.1.182:8041/history/xiazai", value, {
+          responseType: "blob" // 设置响应数据类型
+          //   headers: { "Content-Type": "application/json" }
+        })
+        .then(res => {
+          if (res.status == 200) {
+            console.log(res);
+            var blob = new Blob([res.data], {
+              type: "image/jpeg"
+            });
+            var downloadElement = document.createElement("a");
+            var href = window.URL.createObjectURL(blob); //创建下载的链接
+            downloadElement.href = href;
+
+            downloadElement.download = this.fileName + ".jpg"; //下载后文件名
+            document.body.appendChild(downloadElement);
+            downloadElement.click(); //点击下载
+            document.body.removeChild(downloadElement); //下载完成移除元素
+            window.URL.revokeObjectURL(href); //释放掉blob对象
+          }
+        });
+    },
+    shoWerweima(index, row) {
+      this.axios
+        .get("http://192.168.1.182:8041/history/test?id=" + row.id)
+        .then(res => {
+          if (res.data.code === 200) {
+            this.fileName = row.recipeName;
+            this.erweimaVisible = true;
+            this.erweima = res.data.data.qrCode;
+          }
+          console.log(res);
+        });
+    },
+    changeState(val) {
+      let qs = require("qs");
+      let data = qs.stringify({
+        id: val.id
+      });
+      this.Axios(
+        {
+          params: data,
+          option: {
+            successMsg: "修改成功"
+          },
+          type: "post",
+          url: "/api-recipe/recipe/updatestate",
+          loadingConfig: {
+            target: document.querySelector(".cookbook_list")
+          }
+        },
+        this
+      ).then(
+        result => {
+          if (result.data.code === 200) {
+            this.getCookbookList();
+          }
+        },
+        ({ type, info }) => {}
+      );
+    },
+    changeUp(index, val) {
+      if (val.state == 1) {
+        this.changeState(val);
+      } else {
+        this.$confirm("菜谱下架，绑定的商品也同样会下架, 是否继续?", "提示", {
+          confirmButtonText: "确定",
+          cancelButtonText: "取消",
+          type: "warning",
+          cancelButtonClass: "is-plain"
+        })
+          .then(() => {
+            this.changeState(val);
+          })
+          .catch(() => {});
+      }
+    },
+    handleSizeChange(val) {
+      this.pageIndex = 1;
+      this.pageSize = val;
+      this.getCookbookList();
+    },
+    handleCurrentChange(val) {
+      this.pageIndex = val;
+      this.getCookbookList();
+    },
+    handleEdit(index, row) {
+      this.$router.push({
+        path: "/Cookbook/EditCookbook/" + row.id
+      });
+    },
+    handleSelectionChange(selection) {},
+    getRow(row, event) {},
+    getCookbookList() {
+      let data = {
+        page: this.pageIndex,
+        size: this.pageSize,
+        keyword: this.keyword
+      };
+      this.Axios(
+        {
+          params: data,
+          option: {
+            enableMsg: false
+          },
+          type: "get",
+          url: "/api-recipe/recipe/list",
+          loadingConfig: {
+            target: document.querySelector(".cookbook_list")
+          }
+        },
+        this
+      ).then(
+        result => {
+          if (result.data.code === 200) {
+            this.total = result.data.data.totalElement;
+            this.tableData = result.data.data.content;
+            this.total = result.data.data.totalElement;
+          }
+        },
+        ({ type, info }) => {}
+      );
+    }
+  },
+  created() {
+    this.getCookbookList();
+    let a = this.$route.matched.find(
+      item =>
+        item.name === "AddCookbook" || item.name === "PrefabricatedRecipes"
+    )
+      ? true
+      : false;
+    let b = this.$route.params.id !== undefined ? true : false;
+    this.isHideList = a || b ? true : false;
+  },
+  components: {
+    tableList
+  },
+  watch: {
+    $route() {
+      this.reload();
+      let a = this.$route.matched.find(
+        item =>
+          item.name === "AddCookbook" || item.name === "PrefabricatedRecipes"
+      )
+        ? true
+        : false;
+      let b = this.$route.params.id !== undefined ? true : false;
+      this.isHideList = a || b ? true : false;
+    }
+  }
 };
 </script>
 
@@ -251,35 +307,35 @@ export default {
 @font-special: #1cc09f;
 @border: 1px solid #dde2eb;
 .cookbook_list {
-	font-size: 14px;
-	color: @font-normal;
-	.top_list {
-		// line-height: 60px;
-		background-color: white;
-		padding: 10px;
-	}
-	.bottom_list {
-		background-color: white;
-		margin-top: 10px;
-		padding-bottom: 10px;
-		overflow: hidden;
-		.top_title {
-			padding: 0 10px;
-			line-height: 60px;
-			overflow: hidden;
-			border-bottom: @border;
-			h4 {
-				float: left;
-			}
-			.top_search {
-				width: 400px;
-				float: right;
-			}
-		}
-		.table_list {
-			overflow: hidden;
-			padding: 10px;
-		}
-	}
+  font-size: 14px;
+  color: @font-normal;
+  .top_list {
+    // line-height: 60px;
+    background-color: white;
+    padding: 10px;
+  }
+  .bottom_list {
+    background-color: white;
+    margin-top: 10px;
+    padding-bottom: 10px;
+    overflow: hidden;
+    .top_title {
+      padding: 0 10px;
+      line-height: 60px;
+      overflow: hidden;
+      border-bottom: @border;
+      h4 {
+        float: left;
+      }
+      .top_search {
+        width: 400px;
+        float: right;
+      }
+    }
+    .table_list {
+      overflow: hidden;
+      padding: 10px;
+    }
+  }
 }
 </style>
